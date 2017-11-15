a9ef7549e5d2646675408d5fde97b40cd3c0dc2e
penglong <penglong@staff.weibo.com>
2017-11-15 18:05:48 +0800

    修改打开shadowsock脚本

--- apply/shadowsock.py
+++ apply/shadowsock.py
@@ -3,6 +3,5 @@ import shutil
 
 os.system('open -a /Applications/ShadowsocksX.app')
 os.chdir('/Users/yetu/work/shadowsocks/')
-os.system('sh start_kc.sh')
-
+os.system('echo "521484"|sudo -S ./kcptun-darwin-amd64-20170525/client_darwin_amd64 -r "23.106.145.171:29900" -l ":443" -mode fast3 -key "very fast"')
 print('开启成功')
