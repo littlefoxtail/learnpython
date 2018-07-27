5a18e24633e748b28e4f479174a3fae0a9c8f2b4
penglong <penglong@staff.weibo.com>
2018-07-27 16:37:37 +0800

    移除部分代码

--- apply/start_shadowsock.py
+++ apply/start_shadowsock.py
@@ -3,6 +3,6 @@ import shutil
 
 os.system('open -a /Applications/ShadowsocksX-NG.app')
 os.chdir('/Users/yetu/work/shadowsocks/')
-os.system('echo "521484"|sudo -S ./kcptun-darwin-amd64-20170525/client_darwin_amd64 -r "23.106.145.171:29900" -l ":443" -mode fast3 -key "very fast"')
+os.system('sudo -S ./kcptun-darwin-amd64-20170525/client_darwin_amd64 -r "23.106.145.171:29900" -l ":443" -mode fast3 -key "very fast"')
 
 print('----已经停止运行----')
--- apply/stop_shadowsock.py
+++ apply/stop_shadowsock.py
@@ -9,7 +9,7 @@ for line in out.splitlines():
     if './kcptun-darwin-amd64-20170525/client_darwin_amd64 -r 23.106.145.171:29900 -l :443 -mode fast3 -key very fast'.encode(encoding = 'utf-8') in line:
         pid = int(line.split(None, 1)[0])
         if 'sudo'.encode(encoding = 'utf-8') not in line:
-            os.system('sudo kill -SIGINT %d' % (pid))
+            os.system('sudo -S kill -SIGINT %d' % (pid))
             print('已停止进程：', pid)
 
 os.system("osascript -e 'quit app \"/Applications/ShadowsocksX-NG.app\"'")
